98af8ec5761bab1073cc55329816c21acbf36246
Gaetan Vialon <gaetan.vialon@etu.unice.fr>
2017-03-01 13:28:20 +0100

    Modification centre

---
+++ centre/src/main/java/fr/polytech/ihm/HomeController.java
@@ -0,0 +1,40 @@
+package fr.polytech.ihm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * @author Gaetan Vialon
+ *         Created the 21/02/2017.
+ */
+public class HomeController {
+
+    @FXML
+    private Button AccueilButton;
+
+    @FXML
+    private Button ShopButton;
+
+    @FXML
+    private Button InfosButton;
+
+    @FXML
+    private Button EventButton;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    void mouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    public void initialize(){
+    }
+}
---
+++ centre/src/main/java/fr/polytech/ihm/ShopController.java
@@ -0,0 +1,8 @@
+package fr.polytech.ihm;
+
+/**
+ * @author Gaetan Vialon
+ *         Created the 21/02/2017.
+ */
+public class ShopController {
+}
