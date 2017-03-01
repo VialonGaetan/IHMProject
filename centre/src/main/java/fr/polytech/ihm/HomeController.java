91191769df15fd5275666293b5c0ee4fc3879fb8
Gaetan Vialon <gaetan.vialon@etu.unice.fr>
2017-03-01 15:15:12 +0100

    MAJ

--- centre/src/main/java/fr/polytech/ihm/HomeController.java
+++ centre/src/main/java/fr/polytech/ihm/HomeController.java
@@ -4,11 +4,14 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
@@ -16,14 +19,24 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * @author Gaetan Vialon
  *         Created the 21/02/2017.
  */
-public class HomeController extends Application {
+public class HomeController {
 
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+
+    @FXML
+    private MenuButton chooseButton;
 
     @FXML
     private Button AccueilButton;
@@ -49,7 +62,18 @@ public class HomeController extends Application {
 
         }
         if (event.getSource() == ShopButton) {
-            System.out.println("Salut");
+            String fxmlFile = "/fxml/Shop.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            try {
+                Stage stage=(Stage) ShopButton.getScene().getWindow();
+                Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+                Scene scene = new Scene(rootNode);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         if (event.getSource() == InfosButton) {
             System.out.println("Salut");
@@ -60,32 +84,30 @@ public class HomeController extends Application {
     }
 
     @FXML
-    public void initialize() throws IOException {
+    public void initialize() throws IOException{
         ObservableList<Magasin> magasins = FXCollections.observableArrayList();
         magasins.add(new Magasin(Categorie.ModeEnfant,"Magasin qui vend des vêtements","Magasin1.jpg",new String[3],"www.google.com","Sud"));
         magasins.add(new Magasin(Categorie.ModeFemme,"Magasin qui vend des vêtements","Magasin2.jpg",new String[3],"www.modeFemme.com","Nord"));
         magasins.add(new Magasin(Categorie.Jeux,"Magasin qui vend des jeux","Magasin3.jpg",new String[3],"www.jeux.com","Sud"));
         magasins.add(new Magasin(Categorie.Restauration,"Venez manger pas cher","Magasin4.jpg",new String[3],"www.resto.com","Est"));
         magasins.add(new Magasin(Categorie.Jardin,"Faites un beau jardin","Magasin5.jpg",new String[3],"www.jardin.com","Ouest"));
-    }
 
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage stage) throws Exception {
+        searchButton.setOnMouseClicked(event -> {
 
-        initialize();
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-        String fxmlFile = "/fxml/MainPage.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 1366, 716);
-        stage.setTitle("Hello JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
+            String fxmlFile = "/fxml/Shop.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            try {
+                Stage stage=(Stage) searchButton.getScene().getWindow();
+                Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+                Scene scene = new Scene(rootNode);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 }
