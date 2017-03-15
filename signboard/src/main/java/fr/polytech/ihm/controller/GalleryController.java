277f5fd31e71e5851cfee823f595f466d8c88ea4
Gigallith <paul.dupont@etu.unice.fr>
2017-03-15 10:37:52 +0100

    Gallery Controller

--- signboard/src/main/java/fr/polytech/ihm/controller/GalleryController.java
+++ signboard/src/main/java/fr/polytech/ihm/controller/GalleryController.java
@@ -1,52 +1,74 @@
 package fr.polytech.ihm.controller;
 
+import fr.polytech.ihm.model.Item;
+import fr.polytech.ihm.model.ProduitsPhares;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by user on 13/03/2017.
  */
 public class GalleryController {
 
+    private List<ProduitsPhares> listProduct;
+
 
     @FXML
     private ImageView imageGallery;
-
-    private List<String> listeOfImage;
     private int cpt = 0;
 
     @FXML
-    public void initialize() {
-        listeOfImage = new ArrayList<>();
-        listeOfImage.add("/images/produitsPhares/PF1.jpg");
-        listeOfImage.add("/images/produitsPhares/PF2.jpg");
-        listeOfImage.add("/images/produitsPhares/PF3.jpg");
-        listeOfImage.add("/images/produitsPhares/PF4.jpg");
-        listeOfImage.add("/images/produitsPhares/PF5.jpg");
-        listeOfImage.add("/images/produitsPhares/PF6.jpg");
-        listeOfImage.add("/images/produitsPhares/PF7.jpg");
-        listeOfImage.add("/images/produitsPhares/PF8.jpg");
-        listeOfImage.add("/images/produitsPhares/PF9.jpg");
+    private Label titleGallery;
+
+    @FXML
+    private Label descriGallery;
+
+
+    @FXML
+    public void initialize() throws FileNotFoundException {
+        listProduct = new ArrayList<>();
+        String contentOfJSON = new Scanner(new File(System.getProperty("user.dir")+"/signboard/src/main/resources/json/ProduitsPhares.json")).useDelimiter("\\Z").next();
+        JSONObject jsonObject = new JSONObject(contentOfJSON);
+        JSONArray listItems = jsonObject.getJSONArray("liste");
+        for(int i = 0 ; i<listItems.length() ; i++){
+            JSONObject obj = listItems.getJSONObject(i);
+            ProduitsPhares item = new ProduitsPhares(obj.getString("srcImage"),obj.getString("labelTitleFr"),obj.getString("labelDescriFr"),obj.getString("labelTitleEn"),obj.getString("labelDescriEn"));
+            listProduct.add(item);
+        }
+        imageGallery.setImage(new Image(listProduct.get(cpt).getSrcImage()));
+        titleGallery.setText(listProduct.get(cpt).getLabelTitleFr());
+        descriGallery.setText(listProduct.get(cpt).getLabelDescriFr());
+
     }
 
     @FXML
     public void nextImage() {
         cpt++;
-        if (cpt == listeOfImage.size())
+        if (cpt == listProduct.size())
             cpt = 0;
-        imageGallery.setImage(new Image(listeOfImage.get(cpt)));
+        imageGallery.setImage(new Image(listProduct.get(cpt).getSrcImage()));
+        titleGallery.setText(listProduct.get(cpt).getLabelTitleFr());
+        descriGallery.setText(listProduct.get(cpt).getLabelDescriFr());
     }
 
     @FXML
     public void beforeImage() {
         cpt--;
         if (cpt < 0)
-            cpt = listeOfImage.size() - 1;
-        imageGallery.setImage(new Image(listeOfImage.get(cpt)));
+            cpt = listProduct.size() - 1;
+        imageGallery.setImage(new Image(listProduct.get(cpt).getSrcImage()));
+        titleGallery.setText(listProduct.get(cpt).getLabelTitleFr());
+        descriGallery.setText(listProduct.get(cpt).getLabelDescriFr());
     }
 }
 
--- signboard/src/main/java/fr/polytech/ihm/model/ProduitsPhares.java
+++ signboard/src/main/java/fr/polytech/ihm/model/ProduitsPhares.java
@@ -4,4 +4,38 @@ package fr.polytech.ihm.model;
  * Created by user on 14/03/2017.
  */
 public class ProduitsPhares {
+
+    private String srcImage ;
+    private String labelTitleFr;
+    private String labelDescriFr;
+    private String labelTitleEn;
+    private String labelDescriEn;
+
+    public ProduitsPhares(String srcImage, String labelTitleFr, String labelDescriFr, String labelTitleEn, String labelDescriEn) {
+        this.srcImage = srcImage;
+        this.labelTitleFr = labelTitleFr;
+        this.labelDescriFr = labelDescriFr;
+        this.labelTitleEn = labelTitleEn;
+        this.labelDescriEn = labelDescriEn;
+    }
+
+    public String getSrcImage() {
+        return srcImage;
+    }
+
+    public String getLabelTitleFr() {
+        return labelTitleFr;
+    }
+
+    public String getLabelDescriFr() {
+        return labelDescriFr;
+    }
+
+    public String getLabelTitleEn() {
+        return labelTitleEn;
+    }
+
+    public String getLabelDescriEn() {
+        return labelDescriEn;
+    }
 }
